1b3d44a3d3b982b6c14a036080b34610083c3ac6
AlexTerkhov141 <terehovav52s@gmail.com>
2024-11-21 19:58:10 +0300

    add comments

--- app/src/main/java/com/packt/labaandoid2/Instrument.kt
+++ app/src/main/java/com/packt/labaandoid2/Instrument.kt
@@ -1,5 +1,12 @@
 package com.packt.labaandoid2
 
+/*
+ * Instrument
+ *
+ * Класс, представляющий объект музыкального инструмента.
+ * Используется для хранения информации о каждом элементе списка в RecyclerView.
+ */
+
 data class Instrument(
     val name: String,
     val imageRes: Int
